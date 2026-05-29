e1dc7a69ca6ef25b65ae7f6a2f87c1465bae8cb6
Dev-Moynul <stu.moynulislam@gmail.com>
2026-05-29 22:41:22 +0600

    aded exact solution febonacci numbers

--- febonacci.py
+++ febonacci.py
@@ -1,13 +1,14 @@
-l,r =map(int, input().split())
+l,r = map(int, input().split())
+
 a, b = 0, 1
-if l<=0<=r:
-    print(0)
-if l<=1 <=r:
-    print(1)
-while True:
-    c=a+b
-    if c>r:
-        break
-    if c>=l:
-        print(c)
-    a, b = b, c
+one = False
+while a <= r:
+    if a >= l:
+        if a == 1:
+            if not one:
+                print(1)
+                one = True
+        else:
+            print(a)
+
+    a, b = b, a + b
