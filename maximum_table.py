08dfd46c591682f5e7687f1feebb27cee3b9541b
Dev-Moynul <stu.moynulislam@gmail.com>
2026-06-10 23:18:22 +0600

    cf problem name is maximum table

---
+++ maximum_table.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = [[1] * n for _ in range(n)]
+for i in range(1, n):
+    for j in range(1, n):
+        a[i][j] = a[i - 1][j] + a[i][j - 1]
+print(a[n - 1][n - 1])
