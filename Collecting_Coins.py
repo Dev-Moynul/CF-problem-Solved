783c1e65b73c780245115e48fc4873c446fe942c
Dev-Moynul <stu.moynulislam@gmail.com>
2026-06-11 23:01:52 +0600

    arrenge line

--- Collecting_Coins.py
+++ Collecting_Coins.py
@@ -2,9 +2,12 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
+
     a, b, c, n = map(int, input().split())
+
     mx = max(a, b, c)
     need = (mx - a) + (mx - b) + (mx - c)
+    
     if need <= n and (n - need) % 3 == 0:
         print("YES")
     else:
