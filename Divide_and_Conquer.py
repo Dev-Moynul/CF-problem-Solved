e04b5bd7d3a720bf6a91e320dbc13352aad5b22c
Dev-Moynul <stu.moynulislam@gmail.com>
2026-06-30 21:54:53 +0600

    cf problem name is divide and conquer

---
+++ Divide_and_Conquer.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+
+    if x % y == 0:
+        print("YES")
+    else:
+        print("NO")
