b7bffebef37e75caa5bc9278e8758275843ab00d
Dev-Moynul <stu.moynulislam@gmail.com>
2026-07-05 23:11:38 +0600

    It is Repovive online contest problem name is Blance trio

---
+++ corner_meeting.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    print((2 * n + 2) // 3)
+
+    #"It is Repovive online contest problem name is Blance trio"
